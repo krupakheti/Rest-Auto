7e198eb5723cd7dbb3bd9483e84485921dfab0cd
Karl Smink <krlsmnk@yahoo.com>
2019-04-20 20:34:24 -0500

    Revert "Revert "Stuff added. Highlighter no longer working""
    This reverts commit d60ae8b76aad1ac5328b4a095e641a451fc8b67b.

--- Restaurant Automation/Assets/Scripts/tableInfo.cs
+++ Restaurant Automation/Assets/Scripts/tableInfo.cs
@@ -9,21 +9,29 @@ using System;
 public class tableInfo : MonoBehaviour {
 
     public int tableNumber;
-    public bool occupied, clean, waitingToOrder;
+    public bool occupied =false, clean=true, waitingToOrder=false;
     public List<string> order;
-    public double bill;
+    public double bill = 0;
     public FileStream fileStream;
-
+    public GameObject sceneScriptsPrefab;
+  
     public void markOccupied() {
         occupied = true;
-        waitingToOrder = true;
-        writeToFile();
+        waitingToOrder = true;        
     }
 
-    public void markClean() {
+    public void markClean() {        
+        clean = true;        
+    }
+
+    public void markGuestsLeft() { 
         occupied = false;
-        clean = true;
-        writeToFile();
+        if(order.Count > 0) waitingToOrder = false;
+        clean = false;
+    }
+
+    public void markOrderFilled() { 
+        order.Clear();        
     }
 
     //called when the waitor adds an item to the order
@@ -38,45 +46,37 @@ public class tableInfo : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //open the file, and create defautl table entries if needed
-        //readFromFile();
         string[] tableInformation = readInStrings();
         populateTables(tableInformation);
+
+        sceneScriptsPrefab.SetActive(true);
     }
 
     private void populateTables(string[] tableInformation)
     {
-        for (int i = 1; i < tableInformation.Length; i++) {
+        for (int i = 0; i < 6; i++) {
             parseTableInfo(tableInformation[i], i);
         }
     }
 
-    private void parseTableInfo(string v, int i)
-    {
+    private void parseTableInfo(string thisTableInfo, int currentTable)
+    {        
+        string[] words = thisTableInfo.Split(',');
+        //Debug.Log("Words: " + words[2]);
 
-        /****************************************************THIS IS WHERE YOU WERE WORKING***************************************************/
-        //TODO:
+        //if(int.Parse(words[0]) == tableNumber) { 
+            //tableNumber = int.Parse(words[0]);
+            occupied = bool.Parse(words[1]);
+            clean = bool.Parse(words[2]);
+            waitingToOrder = bool.Parse(words[3]);
+            order = new List<string>(words[4].Split(' '));
+            //order.RemoveAt(order.Count - 1);
+            bill = double.Parse(words[5]);
+        //}
     }
-
-    public void readFromFile() {
-        //look for file with name
-        //if file does't exist: create a file with that name, then open it 
-        //Create new file and open it for read and write, if the file exists throw exception
-
-        //string path = "Assets/Resources/tableInfo.csv";
-        string path = Application.persistentDataPath + "/tableInfo.csv";
-
-        //Read the text from directly from the test.txt file
-        StreamReader reader = new StreamReader(path);
-        reader.ReadToEnd();
-        //Debug.Log(reader.ReadToEnd());
-        reader.Close();
-    }        
-    
-    
-
+        
     public void writeToFile() {
-        //string path = "Assets/Resources/tableInfo.csv";
+        //string path = "Assets/Resources/tableInfo.csv";        
         string path = Application.persistentDataPath + "/newTableInfo.csv";        
         try
         {
@@ -91,9 +91,9 @@ public class tableInfo : MonoBehaviour {
         string[] fileLines = readInStrings(); 
 
         //for each table
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 6; i++) {
             //check if current table is == current line of file (i)
-            if (i == tableNumber)
+            if (i == tableNumber-1)
             {
                 //if it is, write current string to file   
                 string thisTablesInfo = tableNumber + ","
@@ -125,13 +125,14 @@ public class tableInfo : MonoBehaviour {
     }
 
     public string[] readInStrings() {
-        string[] theseStrings = new string[10];
+        string[] theseStrings = new string[6];
         //string path = "Assets/Resources/tableInfo.csv";
+        //Debug.Log(Application.persistentDataPath.ToString());
         string path = Application.persistentDataPath + "/tableInfo.csv";
         StreamReader reader = new StreamReader(path);
 
         //Read the text from directly from the test.txt file
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 6; i++)
         {
             theseStrings[i] = reader.ReadLine();
             //Debug.Log(theseStrings[i]);
@@ -140,14 +141,22 @@ public class tableInfo : MonoBehaviour {
         return theseStrings;
     }
 
-        public void problemAddressed() { 
-            Button thisButton = this.gameObject.GetComponent<Button>();
-            ColorBlock cb = thisButton.colors;
-            if(cb.normalColor == Color.red) {                 
-                cb.normalColor = Color.green;
-                thisButton.colors = cb;
-            } 
-        }
-
+    public void problemAddressed() { 
+        Button thisButton = this.gameObject.GetComponent<Button>();
+        ColorBlock cb = thisButton.colors;
+        if(cb.normalColor == Color.red) {                 
+             cb.normalColor = Color.green;
+             thisButton.colors = cb;
+        }     
+    }
+    
+    public void toString() {         
+        Debug.Log("Table Number: " + tableNumber + "\n");
+        Debug.Log("Occupied: " + occupied + "\n");
+        Debug.Log("Clean: " + clean + "\n");
+        Debug.Log("W2O: " + waitingToOrder + "\n");
+        Debug.Log("Order: " + order + "\n");
+        Debug.Log("Bill: " + bill + "\n");        
+    }
 }
 
